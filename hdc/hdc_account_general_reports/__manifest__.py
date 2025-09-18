bd3defcaebbc0ad0d0e673d9b3a9b82692819cfd
AgentAtnaf <fanta_2545@hotmail.com>
2025-09-18 16:34:23 +0700

    [T25-5031] Add form_no

--- hdc/hdc_account_general_reports/__manifest__.py
+++ hdc/hdc_account_general_reports/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "HDC Account Invoice Reports",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "category": "Invoice Reports",
     "summary": "Account Invoice Reports",
     "description": """
