6c79f119d761e6dc60e37f2678d6bf13a4f94e02
TinMyoWin-lab <tinmyowin11@gmail.com>
2025-09-19 09:50:42 +0700

    [T25-5126] Urgent_Add field dimension on partner scrren. (specific project spe)

---
+++ accounting/bs_finance_dimension_partner_spe/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "bs_finance_dimension_spe",
+    "version": "14.0.1.0.0",
+    "summary": """ To add finance dimension to partner for SPE""",
+    "author": "Basic Solution Co., Ltd.",
+    "website": "https://www.basic-solution.com",
+    "category": "Accounting/Accounting",
+    "depends": ["bs_finance_dimension_spe"],
+    "data": [
+        "views/res_partner_views.xml"
+    ],
+    "application": False,
+    "license": "LGPL-3",
+}
