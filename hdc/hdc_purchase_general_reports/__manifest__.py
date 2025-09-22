ba0ebed99879e850b3e3e2c464ba3145b6462e0f
AgentAtnaf <fanta_2545@hotmail.com>
2025-09-22 13:27:56 +0700

    [T25-5155] Adjust page format

--- hdc/hdc_purchase_general_reports/__manifest__.py
+++ hdc/hdc_purchase_general_reports/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "HDC Purchase Orders Reports",
-    "version": "14.0.2.0.5",
+    "version": "14.0.2.0.6",
     "category": "Purchase Orders Reports",
     "summary": "Purchase Orders Reports",
     "description": """
