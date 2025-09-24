b4a7ef442f8b3ba3012ef4b6fcc6db135bb58306
jacekong <jacekong97@gmail.com>
2025-09-24 09:03:26 +0000

    T25-5156 witholding tax form - v14.0.0.3

--- accounting/bs_l10n_th_withholding_tax_cert_form_spe/__manifest__.py
+++ accounting/bs_l10n_th_withholding_tax_cert_form_spe/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     "name": "BS Withholding Tax SPE",
-    "version": "14.0.0.2",
+    "version": "14.0.0.3",
     'description': """ BS Withholding Tax SPE """,
     'summary': """ BS Withholding Tax SPE  """,
     'author': "Basic Solution Co., Ltd.",
     'website': "https://www.basic-solution.com",
     'category': 'Accounting',
-    'depends': ['bs_withholding_tax_cert_add'],
+    'depends': ['bs_withholding_tax_cert_add', 'l10n_th_withholding_tax_cert_form'],
     "data": [
         "reports/withholding_tax_cert_form_view.xml",
         "views/withholding_tax_cert_views.xml",
