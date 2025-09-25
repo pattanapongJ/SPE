11f3f6edd51d0cfb83d7995c8410234268966a2f
jacekong <jacekong97@gmail.com>
2025-09-25 11:00:28 +0000

    T25-5181 update layout v14.0.1.1.17

--- hdc/hdc_account_general_reports/__manifest__.py
+++ hdc/hdc_account_general_reports/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "HDC Account Invoice Reports",
-    "version": "14.0.1.1.16",
+    "version": "14.0.1.1.17",
     "category": "Invoice Reports",
     "summary": "Account Invoice Reports",
     "description": """
